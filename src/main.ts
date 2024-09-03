c3b8aae5e14e6d7695231367e412940fa6ee4d0c
JonasPalms <jonas.p.sorensen@gmail.com>
2024-09-03 17:25:15 +0200

    add dismissable prop and small styling changes

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,16 @@ let count = 0
 document.querySelector('button')?.addEventListener('click', () => {
   const type = (document.querySelector('#toast-type') as HTMLSelectElement).value
   const position = (document.querySelector('#toast-position') as HTMLSelectElement).value
+  const dissmissable = (document.querySelector('#dissmissable') as HTMLInputElement).checked
 
   count++
 
   new PalmToast({
-    text: 'Hello World!',
     heading: `Toast Heading ${count}`,
+    text: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, quas.',
     position: position as ToastPosition,
     type: type as ToastType,
     duration: 8000,
-    dissmissable: false,
+    dissmissable: dissmissable,
   }).showToast()
 })
